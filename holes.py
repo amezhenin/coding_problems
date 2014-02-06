488ad1b1ede442dcb36879b6d1aceef51f4516f8
Artem Mezhenin <a.mezhenin@gmail.com>
2014-02-06 15:53:37 +0400

    holes problem

---
+++ holes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+"""
+http://www.codechef.com/problems/HOLES
+
+Testing:
+    nosetests --with-doctest <file>
+
+Input:
+2
+CODECHEF
+DRINKEATCODE
+
+Output:
+2
+5
+"""
+
+from collections import defaultdict
+
+HOLES = [(i, 1) for i in "qropad"] + [('b', 2)]
+HOLES = defaultdict(int, HOLES)
+
+
+def alg(a):
+    """
+    >>> alg("CODECHEF")
+    2
+    >>> alg("DRINKEATCODE")
+    5
+    """
+
+    sum = 0
+    for i in a.lower():
+        sum += HOLES[i]
+    return sum
+
+
+if __name__ == "__main__":
+
+    for _ in range(input()):
+        a = raw_input()
+        print alg(a)
