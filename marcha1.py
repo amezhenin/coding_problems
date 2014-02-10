42566d53334a0dfda400cd91f22194761253dea4
Artem Mezhenin <a.mezhenin@gmail.com>
2014-02-10 17:31:15 +0400

    problem march1, correct

--- marcha1.py
+++ marcha1.py
@@ -57,6 +57,42 @@ Yes
 Yes
 No
 Yes
+
+
+Performance tests:
+
+In [2]: %timeit marcha1.alg(132, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+10000 loops, best of 3: 67.8 us per loop
+
+In [3]: %timeit marcha1.alg2(132, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+10000 loops, best of 3: 62 us per loop
+
+In [4]: %timeit marcha1.alg3(132, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+10000 loops, best of 3: 47.6 us per loop
+
+In [5]: %timeit marcha1.alg(13200, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+1 loops, best of 3: 511 ms per loop
+
+In [6]: %timeit marcha1.alg2(13200, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+100 loops, best of 3: 7.48 ms per loop
+
+In [7]: %timeit marcha1.alg3(13200, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+100 loops, best of 3: 4.36 ms per loop
+
+
+I've got different performance results on my other machine. Solutions 2 and 3 are almost equal in speed, but set is
+slightly better in memory consumption:
+
+In [20]: a = {i: True for i in xrange(10000)}
+
+In [21]: b = {i for i in xrange(10000)}
+
+In [24]: sys.getsizeof(a)
+Out[24]: 786712
+
+In [25]: sys.getsizeof(b)
+Out[25]: 524520
+
 """
 
 def read_two(fn=int):
@@ -71,6 +107,8 @@ def read_two(fn=int):
 
 def alg(ask, a, sum=0):
     """
+    My initial solution with recursion.
+
     >>> alg(3, [1, 1, 1])
     True
     >>> alg(11, [1, 2, 4, 8, 16])
@@ -84,7 +122,7 @@ def alg(ask, a, sum=0):
     """
     if ask == sum:
         return True
-    if len(a) == 0:
+    if ask < sum or len(a) == 0:
         return False
 
     if alg(ask, a[1:], sum+a[0]):
@@ -93,6 +131,53 @@ def alg(ask, a, sum=0):
         return True
 
     return False
+
+
+def alg2(ask, a):
+    """
+    Alternative solution with dict. It is based on dynamic programming approach.
+
+    >>> alg2(3, [1, 1, 1])
+    True
+    >>> alg2(11, [1, 2, 4, 8, 16])
+    True
+    >>> alg2(23, [1, 2, 4, 8, 16])
+    True
+    >>> alg2(13, [1, 5, 5, 10, 10])
+    False
+    >>> alg2(132, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+    True
+    """
+
+    sums = {0: True}
+    for x in a:
+        for i in sums.keys():
+            if i+x <= ask:
+                sums[i+x] = True
+
+    return sums.get(ask, False)
+
+
+def alg3(ask, a):
+    """
+    Alternative solution with set. It is based on dynamic programming approach.
+
+    >>> alg3(3, [1, 1, 1])
+    True
+    >>> alg3(11, [1, 2, 4, 8, 16])
+    True
+    >>> alg3(23, [1, 2, 4, 8, 16])
+    True
+    >>> alg3(13, [1, 5, 5, 10, 10])
+    False
+    >>> alg3(132, [17, 6, 4, 998, 254, 137, 259, 153, 154, 3, 28, 19, 123, 542, 857, 23, 687, 35, 99, 999])
+    True
+    """
+
+    sums = {0}
+    for x in a:
+        sums |= {s+x for s in sums if s+x <= ask}
+    return ask in sums
 
 
 if __name__ == "__main__":
