20ebf2c4c53eb8d332ad422b69534f93cf2f81cc
Artem Mezhenin <a.mezhenin@gmail.com>
2014-02-27 20:13:00 +0400

    lepermut problem

---
+++ easy/lepermut.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+"""
+http://www.codechef.com/problems/PERMUT2
+
+Testing:
+    nosetests --with-doctest <file>
+
+
+Input:
+4
+1
+1
+2
+2 1
+3
+3 2 1
+4
+1 3 2 4
+
+Output:
+YES
+YES
+NO
+YES
+"""
+
+
+def read_n_array(fn=int):
+    """
+    Read array of integers with leading N. Optional param can change
+    conversion to function to float.
+    :return tuple (N, list)
+    """
+    n = input()
+    a = map(fn, raw_input().split())
+    return n, a
+
+
+def alg(a):
+    """
+    >>> alg([1])
+    True
+    >>> alg([2, 1])
+    True
+    >>> alg([3, 2, 1])
+    False
+    >>> alg([1, 3, 2, 4])
+    True
+    """
+    '''
+    # Initial (wrong) solution
+    max = a[0]
+    for i in xrange(1, len(a)):
+        if a[i] > max:
+            max = a[i]
+            continue
+        if max != a[i-1]:
+            return False
+
+    return True
+
+
+    # straight forward solution
+    n = len(a)
+    invs = sum([a[i] > a[j] for i in xrange(0, n-1) for j in xrange(i+1, n)])
+    local_invs = sum([a[i] > a[i+1] for i in xrange(0, n-1)])
+    return invs == local_invs
+    '''
+
+    # Best solution
+    for i in a:
+        if (a[i-1]-i)*(a[i-1]-i) > 1:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+
+    for _ in range(input()):
+        _, a = read_n_array()
+        if alg(a):
+            print "YES"
+        else:
+            print "NO"
