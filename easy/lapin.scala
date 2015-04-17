e82d16425f88723293ce5c19d9cb07b0a7ce87ac
Artem Mezhenin <a.mezhenin@gmail.com>
2015-04-17 15:13:19 +0700

    lapin.scala

--- easy/lapin.scala
+++ easy/lapin.scala
@@ -41,6 +41,17 @@ object Main {
     x.deep == y.deep
   }
 
+  /**
+   * Groupby twice as fast as sort
+   * */
+  def alg3(a: String):Boolean = {
+    val s = a.toCharArray
+    val l = s.length
+    val x = s.slice(0, l/2).groupBy(_.toChar).map(p => (p._1, p._2.length))
+    val y = s.slice(l/2+l%2, l).groupBy(_.toChar).map(p => (p._1, p._2.length))
+    x == y
+  }
+
 
   def main(args : Array[String]) = {
     for (i <- 0 until readInt()){
