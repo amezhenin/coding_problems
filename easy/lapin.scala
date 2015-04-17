8bbeb635e005b5dc9a81c48b69be75682a775768
Artem Mezhenin <a.mezhenin@gmail.com>
2015-04-17 14:55:30 +0700

    lapin.scala

---
+++ easy/lapin.scala
@@ -0,0 +1,53 @@
+/**
+ * Problem: http://www.codechef.com/problems/LAPIN
+ * GitHub: https://github.com/amezhenin/codechef_problems
+ */
+object Main {
+
+  /**
+   * Checkout https://github.com/amezhenin/codechef_scala_template to test your solutions with sbt-doctest
+   * {{{
+   * >>> Main.alg("gaga")
+   * true
+   *
+   * >>> Main.alg("abcde")
+   * false
+   *
+   * >>> Main.alg("rotor")
+   * true
+   *
+   * }}}
+   * */
+  def alg(a: String):Boolean = {
+    val s = a.toList
+    val l = s.length
+
+    val x = s.slice(0, l/2).sorted
+    val y = s.slice(l/2+l%2, l).sorted
+
+    x == y
+  }
+
+  /**
+   * Solution with Array[Char] is almost 3 times faster 
+   * */
+  def alg2(a: String):Boolean = {
+    val s = a.toCharArray
+    val l = s.length
+
+    val x = s.slice(0, l/2).sorted
+    val y = s.slice(l/2+l%2, l).sorted
+
+    x.deep == y.deep
+  }
+
+
+  def main(args : Array[String]) = {
+    for (i <- 0 until readInt()){
+      val a = readLine()
+      if (alg(a)) println("YES")
+      else println("NO")
+    }
+
+  }
+}
