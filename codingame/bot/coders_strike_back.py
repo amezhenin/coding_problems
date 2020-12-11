eb628a486e23d5954e307aa12efcc68d680bca78
Artem Mezhenin <a.mezhenin@gmail.com>
2020-12-11 23:11:48 +0400

    CodinGame bot Coders Strike Back: angle and ahead of time turns

--- codingame/bot/coders_strike_back.py
+++ codingame/bot/coders_strike_back.py
@@ -1,19 +1,55 @@
+import sys
 import math
 import collections
+import numpy as np
+from numpy import linalg as LA
+
+"""
+TODO:
+    * attach enemy ahead in race, not closest one
+"""
+
+
+def log(msg):
+    print(msg, file=sys.stderr, flush=True)
+
 
 Bot = collections.namedtuple("Bot", "x y vx vy angle cp_id cp_x cp_y")
 
-COMPENSATION = 3
+COMPENSATION = 2
+CP_RADIUS = 600
+MAX_THRUST = 100
+
+
+def unit_vector(vector):
+    return vector / LA.norm(vector)
+
+
+def deg_to_vector(d):
+    return unit_vector(np.array([np.cos(np.deg2rad(d)), np.sin(np.deg2rad(d))]))
+
+
+def is_same_direction(bot, speed, checkpoint):
+    if LA.norm(speed) == 0 or LA.norm(checkpoint - bot) == 0:
+        return 1.0
+
+    l = LA.norm(unit_vector(checkpoint - bot) + unit_vector(speed))
+    # if l > math.sqrt(2):
+    #     return 1.0
+    # penalty = math.sqrt(2) - l
+    return l > math.sqrt(2)
 
 
 class Game:
     def __init__(self):
+        self.round = 0
         self.laps = int(input())
         cp_count = int(input())
         self.checkpoints = []
         for i in range(cp_count):
             x, y = map(int, input().split())
             self.checkpoints.append((x, y))
+        log(f"{len(self.checkpoints)} checkpoints: {self.checkpoints}")
 
 
     def read_bot(self):
@@ -22,34 +58,73 @@ class Game:
         return bot
 
 
+    def adjust_cp_coord(self, bot):
+        log(f"Bot: {bot}")
+        if bot.vx != 0:
+            t = (bot.cp_x - bot.x) / bot.vx
+        elif bot.vy != 0:
+            t = (bot.cp_y - bot.y) / bot.vy
+        else:
+            log(f"Zero speed")
+            return bot.cp_x, bot.cp_y
+
+        px = (t * bot.vx) + bot.x
+        py = (t * bot.vy) + bot.y
+
+        # minimum distance to checkpoint center
+        dist = LA.norm(np.array([bot.cp_x - px, bot.cp_y - py]))
+        log(f"Min CP dist: {dist} in {t} rounds")
+        if dist < CP_RADIUS:
+            if t < 3:
+                next_cp_id = (bot.cp_id + 1) % len(self.checkpoints)
+                log(f"Switching to CP {next_cp_id}")
+                next_cp = self.checkpoints[next_cp_id]
+                return next_cp[0], next_cp[1]
+            return px, py
+
+
+        nx = bot.cp_x - int(bot.vx * COMPENSATION)
+        ny = bot.cp_y - int(bot.vy * COMPENSATION)
+        return nx, ny
+
+
     def move_to_checkpoint(self, bot):
 
         dist = math.hypot(abs(bot.x - bot.cp_x), abs(bot.y - bot.cp_y))
 
-        # if abs(next_checkpoint_angle) > 90:
-        #     thrust = 0
-        if dist < 1500:
+        same_dir = is_same_direction(
+            np.array([bot.x, bot.y]),
+            # np.array([bot.vx, bot.vy]),
+            deg_to_vector(bot.angle),
+            np.array([bot.cp_x, bot.cp_y])
+        )
+        if self.round == 1:
+            thrust = "BOOST"
+        elif not same_dir:
+            thrust = 0
+        elif dist < 1500:
             thrust = 30
         elif dist < 1050:
             thrust = 20
-        elif dist > 5000:  # and abs(next_checkpoint_angle) < 5:
-            thrust = "BOOST"
         # elif next_checkpoint_dist < 500 and math.hypot(abs(x - opponent_x), abs(y - opponent_y)) < 850:
         #     thrust = "SHIELD"
         else:
             thrust = 100
 
-        nx = bot.cp_x - int(bot.vx * COMPENSATION)
-        ny = bot.cp_y - int(bot.vy * COMPENSATION)
-        print("%s %s %s CP:%s" % (nx, ny, thrust, thrust))
+        nx, ny = self.adjust_cp_coord(bot)
+
+        print("%s %s %s CP:%s" % (int(nx), int(ny), thrust, thrust))
 
 
     def attack(self, bot, en1, en2):
-        d1 = math.hypot(abs(bot.x - en1.x), abs(bot.y - en1.y))
-        d2 = math.hypot(abs(bot.x - en2.x), abs(bot.y - en2.y))
-        enemy, dist = en1, d1
-        if d1 > d2:
-            enemy, dist = en2, d2
+        # d1 = math.hypot(abs(bot.x - en1.x), abs(bot.y - en1.y))
+        # d2 = math.hypot(abs(bot.x - en2.x), abs(bot.y - en2.y))
+        # enemy, dist = en1, d1
+        # if d1 > d2:
+        #     enemy, dist = en2, d2
+        enemy = en1 if en1.cp_id >= en2.cp_id else en2
+        dist = math.hypot(abs(bot.x - enemy.x), abs(bot.y - enemy.y))
+
         nx = enemy.x - int(enemy.vx * COMPENSATION)
         ny = enemy.y - int(enemy.vy * COMPENSATION)
         thrust = "SHIELD" if dist < 1100 else 100
@@ -57,6 +132,8 @@ class Game:
 
 
     def next_round(self):
+        self.round += 1
+
         bot1 = self.read_bot()
         bot2 = self.read_bot()
 
@@ -74,3 +151,4 @@ if __name__ == "__main__":
     game = Game()
     while True:
         game.next_round()
+
