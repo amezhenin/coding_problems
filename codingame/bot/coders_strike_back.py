3f34a782aa0a7163701c25e490487f625c8d617b
Artem Mezhenin <a.mezhenin@gmail.com>
2020-12-12 12:52:16 +0400

    CodinGame bot Coders Strike Back: vectors

--- codingame/bot/coders_strike_back.py
+++ codingame/bot/coders_strike_back.py
@@ -1,6 +1,6 @@
 import sys
 import math
-import collections
+
 import numpy as np
 from numpy import linalg as LA
 
@@ -8,8 +8,6 @@ from numpy import linalg as LA
 def log(msg):
     print(msg, file=sys.stderr, flush=True)
 
-
-Bot = collections.namedtuple("Bot", "x y vx vy angle cp_id cp_x cp_y")
 
 COMPENSATION = 2
 CP_RADIUS = 600
@@ -25,16 +23,61 @@ def deg_to_vector(d):
     return unit_vector(np.array([np.cos(np.deg2rad(d)), np.sin(np.deg2rad(d))]))
 
 
-def is_same_direction(bot, speed, checkpoint):
-    if LA.norm(speed) == 0 or LA.norm(checkpoint - bot) == 0:
-        return 1.0
 
-    l = LA.norm(unit_vector(checkpoint - bot) + unit_vector(speed))
-    # if l > math.sqrt(2):
-    #     return 1.0
-    # penalty = math.sqrt(2) - l
-    return l > math.sqrt(2)
+class Bot:
+    def __init__(self, x, y, vx, vy, angle, cp_id, checkpoints):
+        self.pos = np.array([x, y])
+        self.v = np.array([vx, vy])
+        self.angle = deg_to_vector(angle)
+        self.cp_id = cp_id
+        self.cp = checkpoints[cp_id]
+        self.checkpoints = checkpoints
 
+
+    def __repr__(self):
+        return str(self.pos)
+
+
+    def next_checkpoint(self):
+        """
+        Return checkpoint AFTER next one
+        """
+        idx = (self.cp_id + 1) % len(self.checkpoints)
+        return self.checkpoints[idx]
+
+
+    def is_same_direction(self):
+        if LA.norm(self.cp - self.pos) == 0:
+            return True
+
+        # log(f"Bot: {self.cp} {self.pos} {self.v}")
+        l = LA.norm(unit_vector(self.cp - self.pos) + unit_vector(self.angle))
+        log(f"Bot: {self} Norm {l}")
+        return l > math.sqrt(2)
+
+
+    def adjust_cp_coord(self):
+        # log(f"Bot: {self}")
+        if LA.norm(self.v) != 0:
+            t = min((self.cp - self.pos) / (self.v + 1e-20))
+        else:
+            # log(f"Zero speed")
+            return self.cp, False
+
+        pos = (t * self.v) + self.pos
+
+        # minimum distance to checkpoint center
+        dist = LA.norm(self.cp - pos)
+        # log(f"Min CP dist: {dist} in {t} rounds")
+        if dist < CP_RADIUS:
+            if t < 3.5:
+                # log(f"Switching to CP after next")
+                next_cp = self.next_checkpoint()
+                return next_cp, True
+            return pos, False
+
+        new = self.cp - self.v * COMPENSATION
+        return new, False
 
 
 class Game:
@@ -46,101 +89,66 @@ class Game:
         self.checkpoints = []
         for i in range(cp_count):
             x, y = map(int, input().split())
-            self.checkpoints.append((x, y))
-        log(f"{len(self.checkpoints)} checkpoints: {self.checkpoints}")
+            self.checkpoints.append(np.array((x, y)))
+        # log(f"{len(self.checkpoints)} checkpoints: {self.checkpoints}")
 
 
     def read_bot(self):
         x, y, vx, vy, angle, cp_id = map(int, input().split())
-        bot = Bot(x, y, vx, vy, angle, cp_id, self.checkpoints[cp_id][0], self.checkpoints[cp_id][1])
+        bot = Bot(x, y, vx, vy, angle, cp_id, self.checkpoints)
         return bot
 
 
-    def adjust_cp_coord(self, bot):
-        # log(f"Bot: {bot}")
-        if bot.vx != 0:
-            t = (bot.cp_x - bot.x) / bot.vx
-        elif bot.vy != 0:
-            t = (bot.cp_y - bot.y) / bot.vy
-        else:
-            # log(f"Zero speed")
-            return bot.cp_x, bot.cp_y
-
-        px = (t * bot.vx) + bot.x
-        py = (t * bot.vy) + bot.y
-
-        # minimum distance to checkpoint center
-        dist = LA.norm(np.array([bot.cp_x - px, bot.cp_y - py]))
-        # log(f"Min CP dist: {dist} in {t} rounds")
-        if dist < CP_RADIUS:
-            if t < 3:
-                next_cp_id = (bot.cp_id + 1) % len(self.checkpoints)
-                log(f"Switching to CP {next_cp_id}")
-                next_cp = self.checkpoints[next_cp_id]
-                return next_cp[0], next_cp[1]
-            return px, py
-
-
-        nx = bot.cp_x - int(bot.vx * COMPENSATION)
-        ny = bot.cp_y - int(bot.vy * COMPENSATION)
-        return nx, ny
-
 
     def move_to_checkpoint(self, bot):
+        dist = LA.norm(bot.pos - bot.cp)
 
-        dist = math.hypot(abs(bot.x - bot.cp_x), abs(bot.y - bot.cp_y))
-
-        same_dir = is_same_direction(
-            np.array([bot.x, bot.y]),
-            # np.array([bot.vx, bot.vy]),
-            deg_to_vector(bot.angle),
-            np.array([bot.cp_x, bot.cp_y])
-        )
         if self.round == 1:
             thrust = "BOOST"
-        elif not same_dir:
+        elif not bot.is_same_direction():
             thrust = 0
-        elif dist < 1500:
-            thrust = 40
         elif dist < 1000:
             thrust = 30
-        # elif next_checkpoint_dist < 500 and math.hypot(abs(x - opponent_x), abs(y - opponent_y)) < 850:
-        #     thrust = "SHIELD"
+        elif dist < 1500:
+            thrust = 50
+        elif dist < 2000:
+            thrust = 70
         else:
             thrust = 100
 
         # if self.shield_needed(bot):
         #     thrust = "SHIELD"
 
-        nx, ny = self.adjust_cp_coord(bot)
-
-        print("%s %s %s CP:%s" % (int(nx), int(ny), thrust, thrust))
+        new, is_next_cp = bot.adjust_cp_coord()
+        if is_next_cp:
+            thrust = 0
+        print("%s %s %s CP:%s" % (int(new[0]), int(new[1]), thrust, thrust))
 
 
     def shield_needed(self, bot):
         min_dist = BOT_RADIUS * 100
         for en in self.enemies:
-            d = math.hypot(abs(bot.x + bot.vx - en.x - en.vx), abs(bot.y + bot.vy - en.y - en.vy))
+            # d = math.hypot(abs(bot.x + bot.vx - en.x - en.vx), abs(bot.y + bot.vy - en.y - en.vy))
+            d = LA.norm(bot.pos + bot.v - (en.pos + en.v))
             min_dist = min(min_dist, d)
         log(f"Min enemy dist {min_dist}")
         return min_dist <= (BOT_RADIUS * 2 + MAX_THRUST)
 
 
     def attack(self, bot):
+        """
+        New alg for attack:
+            If enemy closer to next checkpoint, go to his CP after the next one.
+            If you are closer to enemies CP, attack him
+        """
         en1, en2 = self.enemies
-        # d1 = math.hypot(abs(bot.x - en1.x), abs(bot.y - en1.y))
-        # d2 = math.hypot(abs(bot.x - en2.x), abs(bot.y - en2.y))
-        # enemy, dist = en1, d1
-        # if d1 > d2:
-        #     enemy, dist = en2, d2
         enemy = en1 if en1.cp_id >= en2.cp_id else en2
 
-        nx = enemy.x - int(enemy.vx * COMPENSATION)
-        ny = enemy.y - int(enemy.vy * COMPENSATION)
+        new = enemy.pos - enemy.v * COMPENSATION
         thrust = 100
         if self.shield_needed(bot):
             thrust = "SHIELD"
-        print("%s %s %s AT:%s" % (nx, ny, thrust, thrust))
+        print("%s %s %s AT:%s" % (int(new[0]), int(new[1]), thrust, thrust))
 
 
     def next_round(self):
