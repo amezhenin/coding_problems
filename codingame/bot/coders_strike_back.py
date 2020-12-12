5704af62557a179dc6d3a1ecddfae69f567dcb6f
Artem Mezhenin <a.mezhenin@gmail.com>
2020-12-12 15:31:37 +0400

    CodinGame bot Coders Strike Back: attack tuning

--- codingame/bot/coders_strike_back.py
+++ codingame/bot/coders_strike_back.py
@@ -9,7 +9,8 @@ def log(msg):
     print(msg, file=sys.stderr, flush=True)
 
 
-COMPENSATION = 2
+COMPENSATION = 3
+ATTACK_COMPENSATION = 6
 CP_RADIUS = 600
 BOT_RADIUS = 400
 MAX_THRUST = 100
@@ -25,17 +26,24 @@ def deg_to_vector(d):
 
 
 class Bot:
-    def __init__(self, x, y, vx, vy, angle, cp_id, checkpoints):
+    def __init__(self, bot_id, x, y, vx, vy, angle, cp_id, checkpoints, old_bot):
+        self.id = bot_id
         self.pos = np.array([x, y])
         self.v = np.array([vx, vy])
         self.angle = deg_to_vector(angle)
         self.cp_id = cp_id
         self.cp = checkpoints[cp_id]
         self.checkpoints = checkpoints
+        # this is sum of checkpoint IDs to determine who is ahead
+        if old_bot is None:
+            self.cp_sum = 0
+        else:
+            self.cp_sum = old_bot.cp_sum + (old_bot.cp_id != self.cp_id)
 
 
     def __repr__(self):
-        return str(self.pos)
+        point = self.leader_points
+        return f"ID_{self.id} ({point[0]}, {point[1]})"
 
 
     def next_checkpoint(self):
@@ -46,13 +54,13 @@ class Bot:
         return self.checkpoints[idx]
 
 
-    def is_same_direction(self):
-        if LA.norm(self.cp - self.pos) == 0:
+    def is_same_direction(self, point):
+        if LA.norm(point - self.pos) == 0:
             return True
 
         # log(f"Bot: {self.cp} {self.pos} {self.v}")
-        l = LA.norm(unit_vector(self.cp - self.pos) + unit_vector(self.angle))
-        log(f"Bot: {self} Norm {l}")
+        l = LA.norm(unit_vector(point - self.pos) + unit_vector(self.angle))
+        # log(f"Bot: {self} Norm {l}")
         return l > math.sqrt(2)
 
 
@@ -66,11 +74,12 @@ class Bot:
 
         pos = (t * self.v) + self.pos
 
+        # FIXME: there is a bug here :(
         # minimum distance to checkpoint center
         dist = LA.norm(self.cp - pos)
         # log(f"Min CP dist: {dist} in {t} rounds")
         if dist < CP_RADIUS:
-            if t < 3.5:
+            if t < 2.5:
                 # log(f"Switching to CP after next")
                 next_cp = self.next_checkpoint()
                 return next_cp, True
@@ -80,11 +89,31 @@ class Bot:
         return new, False
 
 
+    @property
+    def leader_points(self):
+        dist = LA.norm(self.cp - self.pos)
+        return self.cp_sum, -int(dist)
+
+
+    @staticmethod
+    def sort(bot_a, bot_b):
+        """
+        We sort bots by their leadership in the race
+        """
+        # we take bot with highest CPs passed and lowest distance to next one (if equal)
+        bots = [bot_a, bot_b]
+        bots.sort(reverse=True, key=lambda x: x.leader_points)
+        return bots
+
+
+
 class Game:
     def __init__(self):
         self.round = 0
-        self.enemies = []
+        self.bots = [None, None]
+        self.enemies = [None, None]
         self.laps = int(input())  # FIXME: start using this to determine last checkpoint
+        log(f"Laps: {self.laps}")
         cp_count = int(input())
         self.checkpoints = []
         for i in range(cp_count):
@@ -93,9 +122,9 @@ class Game:
         # log(f"{len(self.checkpoints)} checkpoints: {self.checkpoints}")
 
 
-    def read_bot(self):
+    def read_bot(self, bot_id, old_bot):
         x, y, vx, vy, angle, cp_id = map(int, input().split())
-        bot = Bot(x, y, vx, vy, angle, cp_id, self.checkpoints)
+        bot = Bot(bot_id, x, y, vx, vy, angle, cp_id, self.checkpoints, old_bot)
         return bot
 
 
@@ -105,14 +134,14 @@ class Game:
 
         if self.round == 1:
             thrust = "BOOST"
-        elif not bot.is_same_direction():
+        elif not bot.is_same_direction(bot.cp):
             thrust = 0
-        elif dist < 1000:
-            thrust = 30
+        # elif dist < 1000:
+        #     thrust = 30
         elif dist < 1500:
             thrust = 50
-        elif dist < 2000:
-            thrust = 70
+        # elif dist < 2000:
+        #     thrust = 70
         else:
             thrust = 100
 
@@ -131,21 +160,32 @@ class Game:
             # d = math.hypot(abs(bot.x + bot.vx - en.x - en.vx), abs(bot.y + bot.vy - en.y - en.vy))
             d = LA.norm(bot.pos + bot.v - (en.pos + en.v))
             min_dist = min(min_dist, d)
-        log(f"Min enemy dist {min_dist}")
+        # log(f"Min enemy dist {min_dist}")
         return min_dist <= (BOT_RADIUS * 2 + MAX_THRUST)
 
 
     def attack(self, bot):
         """
-        New alg for attack:
-            If enemy closer to next checkpoint, go to his CP after the next one.
-            If you are closer to enemies CP, attack him
         """
-        en1, en2 = self.enemies
-        enemy = en1 if en1.cp_id >= en2.cp_id else en2
+        # sort enemies by their position in the race
+        enemies = Bot.sort(*self.enemies)
+        log(f"Enemies: {enemies[0]} {enemies[1]}")
+        enemy = enemies[0]
 
-        new = enemy.pos - enemy.v * COMPENSATION
-        thrust = 100
+        # If enemy closer to next checkpoint, go to his CP after the next one.
+        if LA.norm(bot.pos - enemy.cp) > LA.norm(enemy.pos - enemy.cp):
+            new = enemy.next_checkpoint()
+        else:
+            # If you are closer to enemies CP, attack him
+            new = enemy.pos + (enemy.angle * 100 + enemy.v - bot.v) * ATTACK_COMPENSATION
+
+        # new = enemy.pos - enemy.v * COMPENSATION
+        # if self.round == 1:
+        #     thrust = "BOOST"
+        if not bot.is_same_direction(new):
+            thrust = 0
+        else:
+            thrust = 100
         if self.shield_needed(bot):
             thrust = "SHIELD"
         print("%s %s %s AT:%s" % (int(new[0]), int(new[1]), thrust, thrust))
@@ -154,17 +194,17 @@ class Game:
     def next_round(self):
         self.round += 1
 
-        bot1 = self.read_bot()
-        bot2 = self.read_bot()
+        bot1 = self.read_bot(1, self.bots[0])
+        bot2 = self.read_bot(2, self.bots[1])
+        self.bots = [bot1, bot2]  #Bot.sort(bot1, bot2)
+        #log(f"Bots: {self.bots[0]} {self.bots[1]}")
 
-        en1 = self.read_bot()
-        en2 = self.read_bot()
+        en1 = self.read_bot(1, self.enemies[0])
+        en2 = self.read_bot(2, self.enemies[1])
         self.enemies = [en1, en2]
 
-        self.move_to_checkpoint(bot1)
-        #self.move_to_checkpoint(bot2)
-        self.attack(bot2)
-
+        self.move_to_checkpoint(self.bots[0])
+        self.attack(self.bots[1])
 
 
 
