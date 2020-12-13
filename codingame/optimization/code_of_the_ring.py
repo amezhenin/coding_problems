d300ec5447b503b26f0f831f64ec1b6ac8884b1d
Artem Mezhenin <a.mezhenin@gmail.com>
2020-12-13 20:58:43 +0400

    CodinGame optimization Code of the Ring: 6k

--- codingame/optimization/code_of_the_ring.py
+++ codingame/optimization/code_of_the_ring.py
@@ -2,37 +2,58 @@
 https://www.codingame.com/multiplayer/optimization/code-of-the-rings
 """
 import sys
+
+
 def log(msg):
     print(msg, file=sys.stderr, flush=True)
 
 
-word = "AZ" #input()
+def get_alpha_pos(c):
+    if c == " ":
+        return N
+    return ord(c) - ord("A") + 1 + N
+
+A = " ABCDEFGHIJKLMNOPQRSTUVWXYZ" * 3
+N = len(A) // 3
+
+word = input()
 log(word)
 
-s = ""
+res = ""
 forest = [" "] * 30
-p = 0
+cur_pos = 0
 for c in word:
-    try:
-        x = forest.index(c)
-        fs = (x+30 - p) % 30
-        bs = (p+30 - x) % 30
-        if fs <= bs:
-            s += ">" * fs + "."
-            p = (p+fs) % 30
+    # how much actions need to to transform each cell into what we want
+    actions = []
+    for x in range(30):
+        # calc forward and back steps to that cell
+        forward_steps = (x + 30 - cur_pos) % 30
+        back_steps = (cur_pos + 30 - x) % 30
+        if forward_steps < back_steps:
+            steps = ">" * forward_steps
         else:
-            s += "<" * bs + "."
-            p = (p - bs + 30) % 30
-    except:
-        s += ">"
-        p += 1
-        while forest[p] != c:
-            s += "+"
-            if forest[p] == "Z":
-                forest[p] = " "
-            elif forest[p] == " ":
-                forest[p] = "A"
-            else:
-                forest[p] = chr(ord(forest[p]) + 1)
-        s += "."
-print(s)
+            steps = "<" * back_steps
+
+        # calc + and - actions to transform that cell into what we want
+        alpha_pos = get_alpha_pos(forest[x])
+        plus = 0
+        while c != A[alpha_pos + plus]:
+            plus += 1
+
+        alpha_pos = get_alpha_pos(forest[x])
+        minus = 0
+        while c != A[alpha_pos - minus]:
+            minus += 1
+        if plus < minus:
+            acts = "+" * plus
+        else:
+            acts = "-" * minus
+        actions.append((steps + acts + ".", x))
+
+    actions.sort(key=lambda l: len(l[0]))
+    final_steps, idx = actions[0]
+    res += final_steps
+    cur_pos = idx
+    forest[cur_pos] = c
+
+print(res)
