9264597cfc0013c12e0cb843bce51043d503c9e0
Artem Mezhenin <a.mezhenin@gmail.com>
2020-12-15 12:30:34 +0400

    CodinGame separate template for code golf

---
+++ golf_template.py
@@ -0,0 +1,32 @@
+"""
+I,N=input,int
+for i in" "*N(I()):print(f"{N(I()):b}")
+
+# acts like " ".join(array), but can have logic inside
+f=0
+for i in range(n):
+ if <condition>:print(end=" "*f+f"{<something>}");f=1
+
+# another inlined join
+sentence = input().split()
+print(*(sum(map('aeiouAEIOU'.count, i)) for i in sentence))
+
+# import and assign
+C=__import__("collections").Counter
+"""
+
+
+# convert any code into shorter version (len must be even!)
+def encode(code):
+    code += " " if len(code) % 2 else ""
+    bcode = bytes(code, "ASCII")
+    res = bcode.decode("U16")
+    return len(res), res
+# print(encode(\"""<ascii code>\""")
+# usage
+# exec(bytes('<UTF-16 chars>','U16')[2:])
+
+
+code=""""""
+print(len(code))
+print(encode(code))
