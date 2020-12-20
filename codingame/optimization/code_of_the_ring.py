a9126b58fea5a6668745cca4d21d4dcdf4845a73
Artem Mezhenin <a.mezhenin@gmail.com>
2020-12-20 11:21:18 +0400

    CodinGame optimization Code ot the Ring: reset letters with [+]

--- codingame/optimization/code_of_the_ring.py
+++ codingame/optimization/code_of_the_ring.py
@@ -8,10 +8,24 @@ def log(msg):
     print(msg, file=sys.stderr, flush=True)
 
 
-def get_alpha_pos(c):
+def get_alpha_pos(c, with_offset=True):
     if c == " ":
         return N
-    return ord(c) - ord("A") + 1 + N
+    return ord(c) - ord("A") + 1 + (N * with_offset)
+
+
+def repeat_char(start):
+    # NOT IN USE
+    end = start
+    while end < len(word) and word[start] == word[end]:
+        end += 1
+    log(f"{start} {end}")
+
+    if end == len(word):
+        log("Repeating one char")
+        return "[.]"
+    return "."
+
 
 A = " ABCDEFGHIJKLMNOPQRSTUVWXYZ" * 3
 N = len(A) // 3
@@ -22,6 +36,7 @@ log(word)
 res = ""
 forest = [" "] * 30
 cur_pos = 0
+
 for c in word:
     # how much actions need to to transform each cell into what we want
     actions = []
@@ -39,7 +54,6 @@ for c in word:
         plus = 0
         while c != A[alpha_pos + plus]:
             plus += 1
-
         alpha_pos = get_alpha_pos(forest[x])
         minus = 0
         while c != A[alpha_pos - minus]:
@@ -48,6 +62,17 @@ for c in word:
             acts = "+" * plus
         else:
             acts = "-" * minus
+
+        if ord(c) - ord("A") < ord("Z") - ord(c) + 1:
+            alt_acts = "[+]" + "+" * (ord(c) - ord("A") + 1)
+        else:
+            alt_acts = "[+]" + "-" * (ord("Z") - ord(c) + 1)
+        if len(acts) > len(alt_acts):
+            log(f"Shorter acts: {acts} -> {alt_acts}")
+            acts = alt_acts
+
+        #rep = repeat_char(x)
+
         actions.append((steps + acts + ".", x))
 
     actions.sort(key=lambda l: len(l[0]))
