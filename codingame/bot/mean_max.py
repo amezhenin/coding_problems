b7132a12696b4cdedf7d658d326537bbd70d4195
Artem Mezhenin <a.mezhenin@gmail.com>
2020-12-24 20:20:01 +0400

    CodinGame bot Mean Max: reaper and doof skills

--- codingame/bot/mean_max.py
+++ codingame/bot/mean_max.py
@@ -9,6 +9,7 @@ from numpy import linalg as LA
 def log(msg):
     print(msg, file=sys.stderr, flush=True)
 
+
 ARENA = 6000
 TOWN_RADIS = 3000
 SKILL_RANGE = 2000
@@ -21,6 +22,7 @@ def dist(a, b, with_speed=True):
         return LA.norm(a.pos + a.v - b.pos - b.v)
 
     return LA.norm(a.pos - b.pos)
+
 
 class Wreck:
     def __init__(self, unit_id, radius, x, y, extra):
@@ -71,12 +73,15 @@ class Game:
         self.enemies = [Player(), Player()]
         self.wrecks = []
         self.tankers = []
+        self.tars = []
+        self.oils = []
 
         s = np.sqrt(2)
-        points = [[ARENA, 0], [ARENA//s, -ARENA//s],
-                  [0, -ARENA], [-ARENA//s, -ARENA//s],
-                  [-ARENA, 0], [-ARENA//s, ARENA//s],
-                  [0, ARENA], [ARENA//s, ARENA//s]]
+        DR = 4500
+        points = [[DR, 0], [DR//s, -DR//s],
+                  [0, -DR], [-DR//s, -DR//s],
+                  [-DR, 0], [-DR//s, DR//s],
+                  [0, DR], [DR//s, DR//s]]
         self.doof_points = list(map(lambda x: np.array([int(x[0]), int(x[1])]), points))
         self.next_doof_point = 0
 
@@ -85,21 +90,23 @@ class Game:
         self.round += 1
         self.update_state()
 
-
-        print(self.move_reaper())
-
-        skill = self.skill_destroyer()
-        if skill:
-            print(skill)
-        else:
-            print(self.move_destroyer())
-
-        print(self.move_doof())
+        for sf, mf in [(self.skill_reaper, self.move_reaper),
+                       (self.skill_destroyer, self.move_destroyer),
+                       (self.skill_doof, self.move_doof)]:
+            skill = sf()
+            if skill:
+                print(skill)
+                self.me.rage -= SKILL_COST
+            else:
+                print(mf())
+        pass
 
 
     def update_state(self):
         self.wrecks = []
         self.tankers = []
+        self.tars = []
+        self.oils = []
 
         self.me.score = int(input())
         self.enemies[0].score = int(input())
@@ -114,6 +121,8 @@ class Game:
             inputs = input().split()
             # log(inputs)
             """
+            ['138', '5', '-1', '-1', '1000', '4312', '3477', '0', '0', '1', '-1']
+            
               unit_id    unit_type    player    mass    radius      x      y    vx    vy    extra    extra_2
             ---------  -----------  --------  ------  --------  -----  -----  ----  ----  -------  ---------
                     0            0         0     0.5       400  -2679   3862   121  -266       -1         -1
@@ -135,7 +144,7 @@ class Game:
                    23            4        -1    -1         800   -716  -1927     0     0        5         -1
             """
             unit_id = int(inputs[0])
-            unit_type = int(inputs[1])  # 0 (Reaper), 1 (Destroyer), 3 (Tanker), 4 (Wreck)
+            unit_type = int(inputs[1])  # 0 (Reaper), 1 (Destroyer), 3 (Tanker), 4 (Wreck), 5 (Tar), 6 (Oil)
             player = int(inputs[2])
             mass = float(inputs[3])
             radius = int(inputs[4])
@@ -175,12 +184,17 @@ class Game:
                     # we parse Doof
                     doof = Bot(unit_id, mass, radius, x, y, vx, vy)
                     plr.doof = doof
+            elif unit_type == 5:
+                # we are parsing TAR, treat it as a bot for now for simplicity
+                self.tars.append(Bot(unit_id, mass, radius, x, y, vx, vy))
+
+            elif unit_type == 6:
+                # we are parsing OIL, treat it as a bot for now for simplicity
+                self.oils.append(Bot(unit_id, mass, radius, x, y, vx, vy))
+
             else:
-                # we are parsing TAR or OIL
-                assert unit_type in (5, 6)
                 log(inputs)
-                # TODO: store if we have oil or tar (relate to player?)
-                pass
+                assert False
         pass
 
 
@@ -210,12 +224,6 @@ class Game:
         d = dist(bot, w, with_speed=False)
         dv = dist(bot, w)
 
-        # for i in self.wrecks:
-        #     dd = dist(bot, i, with_speed=False)
-        #     if dd < d:
-        #         w = i
-        #         d = dd
-
         if w.r > d:
             # breaking mechanism
             log(f"D {int(d)} ALT {int(dv)}")
@@ -228,6 +236,28 @@ class Game:
         throttle = min(max(throttle, 0), 300)
         p = w.pos - bot.v
         return f"{p[0]} {p[1]} {throttle} T:{throttle} V:{bot.speed}"
+
+
+    def skill_reaper(self):
+        if not self.can_cast_skill():
+            return None
+
+        bot = self.me.reaper
+        ds = [self.enemies[0].destroyer, self.enemies[1].destroyer]
+        tankers = []
+        for t in self.tankers:
+            for tar in self.tars:
+                if dist(t, tar) <= SKILL_RADIUS:
+                    continue
+            if dist(bot, t) <= SKILL_RANGE and dist(t, self.me.destroyer) > 3000:
+                tankers.append(t)
+
+        log(f"Reaper castable tanker: {len(tankers)}")
+        for t in tankers:
+            if dist(t, ds[0]) < 1000 or dist(t, ds[1]) < 1000:
+                pos = t.pos + t.v
+                return f"SKILL {pos[0]} {pos[1]} TAR {pos[0]} {pos[1]}"
+        return None
 
 
     def move_destroyer(self):
@@ -260,7 +290,7 @@ class Game:
         for w in self.wrecks:
             if dist(bot, w) <= SKILL_RANGE:
                 wrecks.append(w)
-        log(f"Castable wrecks: {len(wrecks)}")
+        log(f"Destr castable wrecks: {len(wrecks)}")
         for w in wrecks:
             if dist(w, rprs[0]) < w.r or dist(w, rprs[1]) < w.r:
                 return f"SKILL {w.pos[0]} {w.pos[1]} BOOM {w.pos[0]} {w.pos[1]}"
@@ -279,6 +309,22 @@ class Game:
 
         return f"{t[0]} {t[1]} 300"
 
+
+    def skill_doof(self):
+        if not self.can_cast_skill():
+            return None
+
+        bot = self.me.doof
+        rprs = [self.enemies[0].reaper, self.enemies[1].reaper]
+        wrecks = []
+        for w in self.wrecks:
+            if dist(bot, w) <= SKILL_RANGE:
+                wrecks.append(w)
+        log(f"Doof castable wrecks: {len(wrecks)}")
+        for w in wrecks:
+            if dist(w, rprs[0]) < w.r or dist(w, rprs[1]) < w.r:
+                return f"SKILL {w.pos[0]} {w.pos[1]} OIL {w.pos[0]} {w.pos[1]}"
+        return None
 
 
 if __name__ == "__main__":
