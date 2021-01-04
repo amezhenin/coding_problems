ce8ad8d348622cdf911ba84d57c30b298061df93
Artem Mezhenin <a.mezhenin@gmail.com>
2021-01-04 13:17:18 +0400

    CG Platinum Rift ep2: top 200

--- codingame/bot/rift_ep2.py
+++ codingame/bot/rift_ep2.py
@@ -26,12 +26,13 @@ class Zone:
     def update_pods(self, owner_id, p0, p1, visible, platinum):
         self.owner_id = owner_id
         self.owned = self.my_id == owner_id
+        # FIXME: do we know about enemy pods if it is not visible?
         if self.my_id == 0:
             self.pods, self.enemy = p0, p1
         else:
             self.pods, self.enemy = p1, p0
         self.visible = visible
-        # FIXME: what is the diff with initial pt?
+        # FIXME: enemy base is always visible with pods, can it be used to analyze enemy income?
         if self.visible:
             log(f"Z {self.id} pt: {platinum}")
             self.pt = platinum
@@ -43,6 +44,7 @@ class Game:
         self.round = 0
         self.my_base = None
         self.enemy_base = None
+        self.blitz_attack = False
 
         # player_count: the amount of players (always 2)
         # my_id: my player ID (0 or 1)
@@ -68,6 +70,7 @@ class Game:
         pass
 
 
+
     def next_round(self):
         self.round += 1
 
@@ -81,6 +84,11 @@ class Game:
             log("Map is ready")
             for z in self.zones.values():
                 assert z.move is not None, f"{z.id} no move"
+            blitz = self.count_attack_steps()
+            log(f"Attack distance: {blitz}")
+            if blitz <= 10:
+                self.blitz_attack = True
+
 
         # move
         move = []
@@ -89,10 +97,11 @@ class Game:
                 continue
 
             pods = z.pods
-            for l in z.links:
-                if pods > 0 and not l.owned and l.id != z.move.id:
-                    move.append(f"1 {z.id} {l.id}")
-                    pods -= 1
+            if self.blitz_attack is False:
+                for l in z.links:
+                    if pods > 0 and not l.owned and l.id != z.move.id:
+                        move.append(f"1 {z.id} {l.id}")
+                        pods -= 1
             if pods > 0:
                 move.append(f"{pods} {z.id} {z.move.id}")
 
@@ -128,6 +137,13 @@ class Game:
                     z.move = target
                     q.append(z)
 
+    def count_attack_steps(self):
+        cnt = 0
+        z = self.my_base
+        while z != self.enemy_base:
+            z = z.move
+            cnt += 1
+        return cnt
 
 
 
@@ -135,40 +151,3 @@ if __name__ == "__main__":
     game = Game()
     while True:
         game.next_round()
-
-
-
-"""
-
-# player_count: the amount of players (always 2)
-# my_id: my player ID (0 or 1)
-# zone_count: the amount of zones on the map
-# link_count: the amount of links between all zones
-player_count, my_id, zone_count, link_count = [int(i) for i in input().split()]
-for i in range(zone_count):
-    # zone_id: this zone's ID (between 0 and zoneCount-1)
-    # platinum_source: Because of the fog, will always be 0
-    zone_id, platinum_source = [int(j) for j in input().split()]
-for i in range(link_count):
-    zone_1, zone_2 = [int(j) for j in input().split()]
-
-# game loop
-while True:
-    my_platinum = int(input())  # your available Platinum
-    for i in range(zone_count):
-        # z_id: this zone's ID
-        # owner_id: the player who owns this zone (-1 otherwise)
-        # pods_p0: player 0's PODs on this zone
-        # pods_p1: player 1's PODs on this zone
-        # visible: 1 if one of your units can see this tile, else 0
-        # platinum: the amount of Platinum this zone can provide (0 if hidden by fog)
-        z_id, owner_id, pods_p0, pods_p1, visible, platinum = [int(j) for j in input().split()]
-
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-
-    # first line for movement commands, second line no longer used (see the protocol in the statement for details)
-    print("WAIT")
-    print("WAIT")
-"""
