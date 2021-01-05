925ab9f075b62e2872b497644f148fc1ef91f1a5
Artem Mezhenin <a.mezhenin@gmail.com>
2021-01-05 15:28:21 +0400

    CG Rift ep2 (top 192)

--- codingame/bot/rift_ep2.py
+++ codingame/bot/rift_ep2.py
@@ -7,35 +7,70 @@ def log(msg):
 
 
 POD_COST = 20
-
+EXPLORE_ROUNDS = 5
 
 class Zone:
-    def __init__(self, zid, pt, my_id):
+
+    def __init__(self, zid, my_id):
         self.id = zid
-        self.pt = pt
+        self.pt = None
         self.my_id = my_id
         self.pods = None
         self.enemy = None
         self.owner_id = None
         self.owned = None
         self.pods = None
-        self.move = None
         self.links = []
         self.visible = None
+        self.attack = None
+        self.attack_dist = None
+        self.explored = False
 
-    def update_pods(self, owner_id, p0, p1, visible, platinum):
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+
+    def __repr__(self):
+        return f"Zone {self.id}: pt {self.pt}"
+
+
+    def update_pods(self, owner_id, my_pods, enemy, visible, platinum, round):
         self.owner_id = owner_id
         self.owned = self.my_id == owner_id
-        # FIXME: do we know about enemy pods if it is not visible?
-        if self.my_id == 0:
-            self.pods, self.enemy = p0, p1
-        else:
-            self.pods, self.enemy = p1, p0
+        self.pods = my_pods
         self.visible = visible
         # FIXME: enemy base is always visible with pods, can it be used to analyze enemy income?
         if self.visible:
-            # log(f"Z {self.id} pt: {platinum}")
+            self.enemy = enemy
             self.pt = platinum
+            self.explored = round
+
+
+    def is_uptodate(self, round):
+        return self.explored and self.explored > round - EXPLORE_ROUNDS
+
+    # @property
+    # def lead_to_unexplored(self):
+    #     for l in self.links:
+    #         if not l.explored:
+    #             return True
+    #     return False
+
+
+    def flee(self):
+        q = deque()
+        for l in self.links:
+            q.append((l, l))
+        while len(q) > 0:
+            target, move = q.popleft()
+            for z in target.links:
+                if z.explored and z.pt > 0 and z.owned:
+                    return move
+                if z.explored:
+                    q.append((z, move))
+        log(f"Can't flee from {self}")
+        return None
 
 
 class Game:
@@ -61,7 +96,7 @@ class Game:
             # platinum_source: Because of the fog, will always be 0
             zid, pt = map(int, input().split())
             assert pt == 0
-            self.zones[zid] = Zone(zid, pt, my_id)
+            self.zones[zid] = Zone(zid, my_id)
 
         for i in range(link_count):
             z1, z2 = map(int, input().split())
@@ -70,43 +105,54 @@ class Game:
         pass
 
 
-
     def next_round(self):
         self.round += 1
 
         my_pt = int(input())  # my available Platinum
         for i in range(self.zone_count):
             zid, owner_id, pods_p0, pods_p1, visible, platinum = map(int, input().split())
-            self.zones[zid].update_pods(owner_id, pods_p0, pods_p1, visible, platinum)
+            if self.my_id == 0:
+                pods, enemy = pods_p0, pods_p1
+            else:
+                pods, enemy = pods_p1, pods_p0
+            self.zones[zid].update_pods(owner_id, pods, enemy, visible, platinum, self.round)
 
         if self.round == 1:
             self.build_move_map()
             log("Map is ready")
             for z in self.zones.values():
-                assert z.move is not None, f"{z.id} no move"
+                assert z.attack is not None, f"{z.id} no attack"
+
             blitz = self.count_attack_steps()
             log(f"Attack distance: {blitz}")
-            if blitz <= 10:
+            if blitz <= 8:
                 self.blitz_attack = True
 
-
         # move
-        move = []
+        all_moves = []
         for z in self.zones.values():
             if z.pods == 0:
                 continue
+            move = []
+            for l in z.links:
+                if self.lead_to_unexplored(l) or (l.pt > 0 and not l.owned):
+                    move.append(l)
+            move.sort(key=lambda x: -x.pt)
 
-            pods = z.pods
-            if self.blitz_attack is False:
-                for l in z.links:
-                    if pods > 0 and not l.owned and l.id != z.move.id:
-                        move.append(f"1 {z.id} {l.id}")
-                        pods -= 1
-            if pods > 0:
-                move.append(f"{pods} {z.id} {z.move.id}")
+            for l in z.links:
+                if l not in move and l.owner_id not in (-1, self.my_id):
+                    move.append(l)
+            move.append(z.attack)
+            base_pods = z.pods // len(move)
+            excess = z.pods % len(move)
+            for idx, m in enumerate(move):
+                pds = base_pods
+                pds += (idx < excess)
+                all_moves.append(f"{pds} {z.id} {m.id}")
 
-        if len(move):
-            print(" ".join(move))
+
+        if len(all_moves):
+            print(" ".join(all_moves))
         else:
             print("WAIT")
 
@@ -114,37 +160,44 @@ class Game:
 
 
     def build_move_map(self):
-
         self.my_base = None
         self.enemy_base = None
         for z in self.zones.values():
             if z.pods > 0:
                 self.my_base = z
                 log(f"My base {z.id}")
-            if z.enemy > 0:
+            if z.enemy:
                 self.enemy_base = z
                 log(f"Enemy base {z.id}")
 
         assert self.enemy_base is not None
 
-        q = deque([self.enemy_base])
-
-        # log(f"Starting queue: {len(q)}")
+        # moves to attack enemy base
+        q = deque([(self.enemy_base, 0)])
         while len(q) > 0:
-            target = q.popleft()
+            target, dist = q.popleft()
             for z in target.links:
-                if not z.move:
-                    z.move = target
-                    q.append(z)
+                if not z.attack:
+                    z.attack = target
+                    z.attack_dist = dist + 1
+                    q.append((z, dist + 1))
 
     def count_attack_steps(self):
         cnt = 0
         z = self.my_base
         while z != self.enemy_base:
-            z = z.move
+            z = z.attack
             cnt += 1
         return cnt
 
+    def lead_to_unexplored(self, zone):
+        if zone == self.enemy_base:
+            return True
+
+        for l in zone.links:
+            if not l.is_uptodate(self.round):
+                return True
+        return False
 
 
 if __name__ == "__main__":
