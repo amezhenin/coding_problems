a38a1f057a51863ac6209dbe260dec470dceda80
Artem Mezhenin <a.mezhenin@gmail.com>
2021-01-08 12:37:27 +0400

    CG puzzle Bender ep 1: 100%

--- codingame/puzzle/medium/bender_1.py
+++ codingame/puzzle/medium/bender_1.py
@@ -17,18 +17,20 @@ def log(msg):
 dirs = ["SOUTH", "EAST", "NORTH", "WEST"]
 def next_dir(cur_dir):
     for i in [cur_dir] + dirs:
-        if i == "SOUTH" and m[l+1][c] in " $SNEW":
+        if i == "SOUTH" and (m[l+1][c] in " $SNEWBIT" or (m[l+1][c] == "X" and breaker)):
             return i
-        elif i == "EAST" and m[l][c+1] in " $SNEW":
+        elif i == "EAST" and (m[l][c+1] in " $SNEWBIT" or (m[l][c+1] == "X" and breaker)):
             return i
-        elif i == "NORTH" and m[l-1][c] in " $SNEW":
+        elif i == "NORTH" and (m[l-1][c] in " $SNEWBIT" or (m[l-1][c] == "X" and breaker)):
             return i
-        elif i == "WEST" and m[l][c-1] in " $SNEW":
+        elif i == "WEST" and (m[l][c-1] in " $SNEWBIT" or (m[l][c-1] == "X" and breaker)):
             return i
     assert False
 
 lines, C = [int(i) for i in input().split()]
 m = []
+tp = []
+tp_dict = {}
 l, c = 0, 0
 for i in range(lines):
     row = list(input())
@@ -38,9 +40,16 @@ for i in range(lines):
         if row[j] == "@":
             row[j] = " "
             l, c = i, j
+        elif row[j] == "T":
+            tp.append((i, j))
 
 log(f"Pos {l} {c}")
+if len(tp) > 0:
+    assert len(tp) == 2
+    tp_dict[tp[0]] = tp[1]
+    tp_dict[tp[1]] = tp[0]
 
+breaker = False
 d = "SOUTH"
 steps = []
 for _ in range(1000):
@@ -59,6 +68,14 @@ for _ in range(1000):
         d = "WEST"
     elif p == "E":
         d = "EAST"
+    elif p == "B":
+        breaker = not breaker
+    elif p == "X":
+        m[l][c] = " "
+    elif p == "I":
+        dirs = dirs[::-1]
+    elif p == "T":
+        l, c = tp_dict[(l, c)]
 
     d = next_dir(d)
     steps.append(d)
