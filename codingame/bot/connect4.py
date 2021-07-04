5d480c230206865b5e5ba1867976300071166caf
Artem Mezhenin <a.mezhenin@gmail.com>
2021-07-04 22:14:12 +0300

    CG bot for Connect-4: first version of MCST + Python
    * Top 1 in wood2 league, but doesn't beat the BOSS

--- codingame/bot/connect4.py
+++ codingame/bot/connect4.py
@@ -1,9 +1,147 @@
 """
 https://www.codingame.com/multiplayer/bot-programming/connect-4
 """
-import sys
+
+TIMELIMIT = 100
+DEPTH = 100  # max is 63
+
+""" ===============================   MCST algorithm   ==============================="""
+
+# from __future__ import division
+
+import time
+import math
 import random
 
+
+def randomPolicy(state):
+    while not state.isTerminal():
+        try:
+            action = random.choice(state.getPossibleActions())
+        except IndexError:
+            raise Exception("Non-terminal state has no possible actions: " + str(state))
+        state = state.takeAction(action)
+    return state.getReward()
+
+
+def limitPolicy(state):
+    i = 0
+    while state.isTerminal() is False and i < DEPTH:
+        try:
+            action = random.choice(state.getPossibleActions())
+        except IndexError:
+            raise Exception("Non-terminal state has no possible actions: " + str(state))
+        state = state.takeAction(action)
+        i += 1
+    return state.getReward()
+
+
+class treeNode:
+    def __init__(self, state, parent):
+        self.state = state
+        self.isTerminal = state.isTerminal()
+        self.isFullyExpanded = self.isTerminal
+        self.parent = parent
+        self.numVisits = 0
+        self.totalReward = 0
+        self.children = {}
+
+    def __str__(self):
+        s=[]
+        s.append("totalReward: %s"%(self.totalReward))
+        s.append("numVisits: %d"%(self.numVisits))
+        s.append("isTerminal: %s"%(self.isTerminal))
+        s.append("possibleActions: %s"%(self.children.keys()))
+        return "%s: {%s}"%(self.__class__.__name__, ', '.join(s))
+
+class MCST:
+    def __init__(self, timeLimit=None, iterationLimit=None, explorationConstant=1 / math.sqrt(2),
+                 rolloutPolicy=randomPolicy):
+        if timeLimit != None:
+            if iterationLimit != None:
+                raise ValueError("Cannot have both a time limit and an iteration limit")
+            # time taken for each MCTS search in milliseconds
+            self.timeLimit = timeLimit
+            self.limitType = 'time'
+        else:
+            if iterationLimit == None:
+                raise ValueError("Must have either a time limit or an iteration limit")
+            # number of iterations of the search
+            if iterationLimit < 1:
+                raise ValueError("Iteration limit must be greater than one")
+            self.searchLimit = iterationLimit
+            self.limitType = 'iterations'
+        self.explorationConstant = explorationConstant
+        self.rollout = rolloutPolicy
+
+    def search(self, initialState, needDetails=False):
+        self.root = treeNode(initialState, None)
+
+        if self.limitType == 'time':
+            timeLimit = time.time() + self.timeLimit / 1000
+            while time.time() < timeLimit:
+                self.executeRound()
+        else:
+            for i in range(self.searchLimit):
+                self.executeRound()
+
+        bestChild = self.getBestChild(self.root, 0)
+        action=(action for action, node in self.root.children.items() if node is bestChild).__next__()
+        if needDetails:
+            return {"action": action, "expectedReward": bestChild.totalReward / bestChild.numVisits}
+        else:
+            return action
+
+    def executeRound(self):
+        """
+            execute a selection-expansion-simulation-backpropagation round
+        """
+        node = self.selectNode(self.root)
+        reward = self.rollout(node.state)
+        self.backpropogate(node, reward)
+
+    def selectNode(self, node):
+        while not node.isTerminal:
+            if node.isFullyExpanded:
+                node = self.getBestChild(node, self.explorationConstant)
+            else:
+                return self.expand(node)
+        return node
+
+    def expand(self, node):
+        actions = node.state.getPossibleActions()
+        for action in actions:
+            if action not in node.children:
+                newNode = treeNode(node.state.takeAction(action), node)
+                node.children[action] = newNode
+                if len(actions) == len(node.children):
+                    node.isFullyExpanded = True
+                return newNode
+
+        raise Exception("Should never reach here")
+
+    def backpropogate(self, node, reward):
+        while node is not None:
+            node.numVisits += 1
+            node.totalReward += reward
+            node = node.parent
+
+    def getBestChild(self, node, explorationValue):
+        bestValue = float("-inf")
+        bestNodes = []
+        for child in node.children.values():
+            nodeValue = node.state.getCurrentPlayer() * child.totalReward / child.numVisits + explorationValue * math.sqrt(
+                2 * math.log(node.numVisits) / child.numVisits)
+            if nodeValue > bestValue:
+                bestValue = nodeValue
+                bestNodes = [child]
+            elif nodeValue == bestValue:
+                bestNodes.append(child)
+        return random.choice(bestNodes)
+
+
+""" ===============================   END OF MCST algorithm   ==============================="""
+import sys
 
 def log(msg):
     print(msg, file=sys.stderr, flush=True)
@@ -54,12 +192,14 @@ class State:
                 if new_board[i][move] == ".":
                     c = str(self.turn % 2)
                     new_board[i] = new_board[i][:move] + c + new_board[i][move + 1:]
+                    break
 
         return State(self.my_id, self.turn + 1, new_board)
 
 
     def isTerminal(self):
-        return self.getReward() != 0
+        res = self.getReward() != 0 or self.getPossibleActions() == []
+        return res
 
 
     def getReward(self):
@@ -154,8 +294,13 @@ class Game:
         self.my_id, self.opp_id = [int(i) for i in input().split()]
         log(f"My ID: {self.my_id}")
 
+        self.mcts = MCST(timeLimit=TIMELIMIT)
+
+
 
     def next_turn(self):
+        t = time.time()
+
         # starts from 0; As the game progresses, first player gets [0,2,4,...] and second player gets [1,3,5,...]
         self.turn = int(input())
 
@@ -178,17 +323,39 @@ class Game:
         # opponent's previous chosen column index (will be -1 for first player in the first turn)
         opp_previous_action = int(input())
 
-        if -2 in actions:
-            print(-2)
-        else:
-            r = random.choice(actions)
-            print(f"{r}")
+        best = self.mcts.search(initialState=state)
+        t = int((time.time() - t) * 1000)
+        log(f"Time: {t}")
+        print(f"{best.move} {t}")
+
+
+def test_mcst_1():
+    board = [
+        ".........",
+        ".........",
+        "...0.....",
+        "...0.0...",
+        "...0.1...",
+        ".1.100...",
+        "101011..."
+    ]
+    state = State(0, 10, board)
+    # state = State(1, 11, board)
+    # mcts = MCST(iterationLimit=1000, rolloutPolicy=limitPolicy)
+    mcts = MCST(timeLimit=100, rolloutPolicy=limitPolicy)
+
+    best = mcts.search(initialState=state)
+    assert best.move == 3
 
 
 if __name__ == "__main__":
+    # test_mcst_1()
     game = Game()
     while True:
         game.next_turn()
+
+
+
 
 
 """
